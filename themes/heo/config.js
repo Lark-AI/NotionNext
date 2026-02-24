41f2889efb8bd6ab8765ab3afcdde1f3878892df
Lark-AI <51391394+Lark-AI@users.noreply.github.com>
2026-02-24 22:02:07 +0800

    Update config.js
    -

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,14 +28,14 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '精选案例', url: '/tag/案例' },
+  HEO_HERO_CATEGORY_2: { title: '人性故事', url: '/tag/故事' },
+  HEO_HERO_CATEGORY_3: { title: '职场规则', url: '/tag/职场' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://bing.img.run/uhd.php', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'http://yueji.org/img/bgimg.png', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
