0fd42b065303976cae83bc2230111ad52f8d87eb
Lark-AI <51391394+Lark-AI@users.noreply.github.com>
2026-02-25 17:27:05 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,11 +7,11 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered By YUEJI.ORG</span>
+      <span className='mr-1'>Powered by</span>
       <a
         href='https://yueji.org'
         className='underline justify-start'>
-        &nbsp;
+        YUEJI.ORG {siteConfig('VERSION')}
       </a>
     </div>
   )
